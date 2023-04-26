b14bab27660f1538a7c4fbeb49d0813c4a85ca7e
albert kipchirchir <albertkipchirchir02@gmail.com>
2023-04-26 16:21:42 +0300

    Delete product implementation

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const conn = mysql.createConnection({
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("views/styles")); // serve static files through this folder
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log("middleware");
@@ -43,8 +44,50 @@ app.get("/new-product", (req, res) => {
 });
 
 app.post("/new-product", (req, res) => {
-  // logic cb func
-  res.send("Product added succefully");
+  conn.query(
+    "INSERT INTO products(name,description,price,quantity,company_id) VALUES(?,?,?,?,?)",
+    [
+      req.body.name,
+      req.body.description,
+      req.body.price,
+      req.body.quantity,
+      req.body.company,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send("Server error");
+      } else {
+        res.send(
+          "Product added succefully. Go <a href='/'>Home</a> or <a href='/new-product'>add another</a> "
+        );
+        // res.redirect("/")
+      }
+    }
+  );
+});
+
+app.get("/delete/:id", (req, res) => {
+  // query paramameters
+  console.log(typeof req.params.id);
+  conn.query(
+    "DELETE FROM products WHERE product_id = ?",
+
+    [Number(req.params.id)],
+
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        res.send("Server Error");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.get("/edit", (req, res) => {
+  res.render("edit");
 });
 
 app.listen(3005, () => console.log("listening on port 3005"));
